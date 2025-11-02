adb6c0b8898203a6209496e4dcbc10dc618fda6c
your_username <malahat.m82@gmail.com>
2025-11-02 22:40:56 +0100

    feat(store): add Zustand channel store with private/public channel support.

---
+++ src/store/channelStore.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+
+interface Channel {
+  name: string;
+  isPrivate: boolean;
+}
+
+interface ChannelState {
+  channels: Channel[];
+  currentChannel: string | null;
+  setChannels: (channels: Channel[]) => void;
+  setCurrentChannel: (name: string) => void;
+}
+
+export const useChannelStore = create<ChannelState>((set) => ({
+  channels: [
+    { name: "#Coders group", isPrivate: false },
+    { name: "#Funny group", isPrivate: false },
+    { name: "#group1", isPrivate: true },
+    { name: "#group2", isPrivate: true },
+  ],
+  currentChannel: null,
+  setChannels: (channels) => set({ channels }),
+  setCurrentChannel: (name) => set({ currentChannel: name }),
+}));
