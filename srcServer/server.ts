deb85c2405c902d511162bceb6abe1dbdfc86709
your_username <malahat.m82@gmail.com>
2025-11-06 11:22:58 +0100

    Adding some comments in server.ts

--- srcServer/server.ts
+++ srcServer/server.ts
@@ -7,32 +7,28 @@ import messages from "./routes/messages.js";
 import login from "./auth/login.js";
 import register from "./auth/register.js";
 
-
-
-
-
 const app = express();
 const PORT = process.env.PORT || 10000;
 
+//  Middlewares
 app.use(cors());
 app.use(express.json());
 
-
-
+//  Logger
 app.use((req, res, next) => {
-  console.log( `${req.method} ${req.url}`);
+  console.log(`${req.method} ${req.url}`);
   next();
 });
 
-
+//  Routes
 app.use("/api/users", users);
 app.use("/api/channels", channels);
 app.use("/api/messages", messages);
 app.use("/api/auth/login", login);
 app.use("/api/auth/register", register);
 
-
-console.log("🔄 Server starting...");
+//  Server start
+console.log(" Server starting...");
 app.listen(PORT, () => {
-  console.log(` Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
